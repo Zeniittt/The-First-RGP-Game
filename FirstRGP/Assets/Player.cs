8177594ac99e5be26b32cda992231871848b32bc
Zeniittt <tinnqd1702@gmail.com>
2023-06-10 09:34:34 +0700

    Animation Idle and Move

--- FirstRGP/Assets/Player.cs
+++ FirstRGP/Assets/Player.cs
@@ -11,9 +11,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
-    [SerializeField] private bool isMoving;
     private float xInput;
-    
+
+    private int facingDirection = 1;
+    private bool facingRight = true;
+
+    [Header("Collision Ground Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +31,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxis("Horizontal");
+        Movement();
+
+        CheckInput();
+
+        CheckCollisionGround();
+
+        FlipController();
+
+        AnimatorControllers();
+    }
+
+    private void Movement()
+    {
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-        if(Input.GetKeyDown(KeyCode.Space))
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
 
-
-        isMoving = rb.velocity.x != 0;
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
         //Dòng code ngay bên d??i là gi?i thích d? hi?u nh?t cho dòng code ngay phía trên
         /*if(rb.velocity.x != 0)
         {
@@ -42,5 +76,35 @@ public class Player : MonoBehaviour
             isMoving = false;
         }*/
         anim.SetBool("isMoving", isMoving);
+        //anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void FlipFacing()
+    {
+        facingDirection = facingDirection * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            FlipFacing();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            FlipFacing();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+    private void CheckCollisionGround()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 }
